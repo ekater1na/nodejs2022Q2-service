752a00836a82207dfdc618122df8a4fba8ca2445
ekater1na <ekaterina_l@tut.by>
2022-07-11 23:03:03 +0300

    build: add .env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const PORT = process.env.PORT || 4000;
+  await app.listen(PORT);
+  console.log(`
+  🚀 Server running on http://localhost:${PORT}`);
 }
 bootstrap();
