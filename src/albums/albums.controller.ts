e6414fc601c2923d42366fb7456c4532691b6b2c
ekater1na <ekaterina_l@tut.by>
2022-07-14 20:30:37 +0300

    feat: update album module

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -6,37 +6,53 @@ import {
   Param,
   Delete,
   Put,
+  HttpCode,
+  HttpStatus,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { AlbumsService } from './albums.service';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { Album } from './interfaces/album.interface';
 
 @Controller('album')
 export class AlbumsController {
   constructor(private readonly albumsService: AlbumsService) {}
 
   @Post()
-  create(@Body() createAlbumDto: CreateAlbumDto) {
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createAlbumDto: CreateAlbumDto): Promise<Album> {
     return this.albumsService.create(createAlbumDto);
   }
 
   @Get()
-  findAll() {
+  @HttpCode(HttpStatus.OK)
+  async findAll(): Promise<Album[]> {
     return this.albumsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.albumsService.findOne(+id);
+  @HttpCode(HttpStatus.OK)
+  async findOne(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+  ): Promise<Album> {
+    return this.albumsService.findOne(id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateAlbumDto: UpdateAlbumDto) {
-    return this.albumsService.update(+id, updateAlbumDto);
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+    @Body() updateAlbumDto: UpdateAlbumDto,
+  ): Promise<Album> {
+    return this.albumsService.update(id, updateAlbumDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.albumsService.remove(+id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string,
+  ): Promise<void> {
+    return this.albumsService.remove(id);
   }
 }
--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
+import { Album } from './interfaces/album.interface';
 
 @Injectable()
 export class AlbumsService {
-  create(createAlbumDto: CreateAlbumDto) {
-    return 'This action adds a new album';
+  private albums: Array<Album> = [];
+
+  create(createAlbumDto: CreateAlbumDto): Album {
+    const newAlbum: Album = {
+      id: uuidv4(),
+      ...createAlbumDto,
+    };
+
+    this.albums.push(newAlbum);
+    return newAlbum;
   }
 
-  findAll() {
-    return `This action returns all albums`;
+  findAll(): Album[] {
+    return this.albums;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} album`;
+  findOne(id: string): Album {
+    const album: Album = this.albums.find((a) => a.id === id);
+
+    if (!album) {
+      throw new NotFoundException('Album not found.');
+    }
+
+    return album;
   }
 
-  update(id: number, updateAlbumDto: UpdateAlbumDto) {
-    return `This action updates a #${id} album`;
+  update(id: string, updateAlbumDto: UpdateAlbumDto): Album {
+    const index: number = this.albums.findIndex((album) => album.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException('Album not found.');
+    }
+
+    const newAlbum: Album = {
+      id,
+      ...updateAlbumDto,
+    };
+
+    this.albums[index] = newAlbum;
+
+    return newAlbum;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} album`;
+  remove(id: string) {
+    const index: number = this.albums.findIndex((album) => album.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException('Album not found.');
+    }
+
+    this.albums.splice(index, 1);
   }
 }
