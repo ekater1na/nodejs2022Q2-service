563d6d873764d86cc9c24d6acaf3f72cc69763fb
ekater1na <ekaterina_l@tut.by>
2022-08-06 22:11:02 +0300

    feat: update auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -69,7 +69,7 @@ export class AuthService {
     const secret = this.config.get('JWT_SECRET_KEY');
 
     const token = await this.jwt.signAsync(payload, {
-      expiresIn: '15m',
+      expiresIn: this.config.get('TOKEN_EXPIRE_TIME'),
       secret: secret,
     });
 
